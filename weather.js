340e03adc419a0688e53ff18072a48ee970f9969
estivedelgado16 <138074670+estivedelgado16@users.noreply.github.com>
2024-02-27 22:03:20 -0500

    up date css

--- weather.js
+++ weather.js
@@ -1,5 +1,6 @@
 document.getElementById('weatherForm').addEventListener('submit', async (e) => {
     e.preventDefault(); 
+    document.querySelector('.target').classList.add('query-submitted');
     const city = document.getElementById('inputCity').value;
     try {
         const response = await fetch(`http://localhost:4001/weather?city=${city}`);
@@ -9,11 +10,11 @@ document.getElementById('weatherForm').addEventListener('submit', async (e) => {
         const weatherData = await response.json();
         const weatherInfo = document.getElementById('weatherInfo');
         weatherInfo.innerHTML = `
-            <h2>${weatherData.name}</h2>
-            <p>Temperature: ${weatherData.main.temp} °C</p>
-            <p>Humidity: ${weatherData.main.humidity}%</p>
-            <p>Pressure: ${weatherData.main.pressure} hPa</p>
-            <p>Wind Speed: ${weatherData.wind.speed} km/h</p>
+            <h2 class="name">${weatherData.name}</h2>
+            <p>Temperatura: ${weatherData.main.temp} °C</p>
+            <p>Humedad: ${weatherData.main.humidity}%</p>
+            <p>Presion: ${weatherData.main.pressure} hPa</p>
+            <p>velocidad del viento: ${weatherData.wind.speed} km/h</p>
         `;
     } catch (error) {
         console.error("Error fetching weather data:", error);
